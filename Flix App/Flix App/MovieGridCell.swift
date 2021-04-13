dba42275c21d88ef12061d47a452f3d42249e3f4
Surabhi <surabhi.rajesh2@gmail.com>
2021-04-13 12:06:36 -0700

    Made changes to HousingForUCMStudents

---
+++ Flix App/Flix App/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix App
+//
+//  Created by Surabhi R on 2/10/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
